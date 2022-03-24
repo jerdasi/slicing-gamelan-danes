83d88a482831450723918ca8ae9bc45b957b326a
jerdasi <jeremiadansil@gmail.com>
2022-03-24 16:50:13 +0700

    done pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 let filterMenu = ["category-filter-all", "newest"]
-let heightCard = [72, 80, 88]
+let heightCard = ["small", "mid", "big"]
+let globalDataGallery = []
+let changingDataGallery = []
 
 // Animation for Hover Card
 const hoverCardInitialize = () => {
@@ -22,16 +24,19 @@ document.querySelector('.filter-button').addEventListener('click', () => {
 document.querySelector('.close-sidebar').addEventListener('click', () => {
     document.querySelector('.sidebar').classList.add('hidden')
 })
+document.querySelector('.apply-button').addEventListener('click', () => {
+    document.querySelector('.sidebar').classList.add('hidden')
+})
 
 // Perhatikan Ini untuk Gallery dengan Jumlah Item Dikit
-FlexMasonry.init('.gallery-content', {
-    responsive: true,
-    breakpointCols: {
-        'min-width: 1024px': 4,
-        'min-width: 640px': 2,
-    },
-    numCols: 2
-});
+// FlexMasonry.init('.gallery-content', {
+//     responsive: true,
+//     breakpointCols: {
+//         'min-width: 1024px': 4,
+//         'min-width: 640px': 2,
+//     },
+//     numCols: 2
+// });
 
 let menu = document.querySelector('.menu')
 
@@ -51,7 +56,11 @@ function toggleMenu(event) {
 
 // Search Function
 document.querySelector("#input-search").addEventListener('keyup', function(event) {
-    console.log(this.value)
+    let result = applyFilter()
+    result = this.value == "" ? [...result] : result.filter(elemen => elemen.nama.toLowerCase().includes(this.value.toLowerCase()))
+    let paginationNumber = document.querySelector(".pagination-number")
+    paginationNumber.parentNode.replaceChild(initPageNumber(result.length), paginationNumber)
+    appendToContainer(paginationTest(1, result))
 })
 
 // Filter by Category
@@ -76,8 +85,8 @@ document.getElementsByName("filter-sort").forEach((element) => {
 // })
 
 
-const buildCardGallery = (url, nama, jenis, tangal) => {
-    let height = heightCard[getRandomInt(0, heightCard.length)]
+const buildCardGallery = (url, nama, jenis, tangal, height) => {
+
     return `
     <div class="w-full px-2 md:px-4 pb-4 h-fit gallery-item relative">
         <div class="item-picture w-full h-fit md:h-${height} hover:cursor-pointer ${jenis} relative">
@@ -104,7 +113,34 @@ const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log("ini belum ready")
+// Membuat Page Number berdasarkan banyaknya data
+const initPageNumber = (total) => {
+    let menu = document.createElement("ul")
+    menu.classList.add("flex", "justify-between", "cursor-pointer", "pagination-number")
+    for (let i = 1; i <= Math.ceil(total / 10); i++) {
+        let list = document.createElement("li")
+
+        // Event Listener
+        list.addEventListener("click", function() {
+            let parent = this.parentNode.childNodes
+            let onPage = this.innerHTML
+            parent.forEach((element, index) => {
+                if (index + 1 == onPage) {
+                    element.classList.add("active")
+                } else {
+                    element.classList.remove("active")
+                }
+            })
+            appendToContainer(paginationTest(this.innerHTML, changingDataGallery))
+                // console.log(this.innerHTML)
+        })
+
+        list.innerText = i
+        i == 1 ? list.classList.add("mx-2", "active") : list.classList.add("mx-2")
+        menu.insertAdjacentElement("beforeend", list)
+    }
+    return menu
+}
 
 // async function fetchingData(){
 //     let response = await fetch("./_data/gamelan.json")
@@ -115,18 +151,58 @@ console.log("ini belum ready")
 // document.addEventListener("DOMContentLoaded", function() {
 //     // Fetch Data
 
+// Sort data berdasarkan key
+const sortItem = (data, key) => {
+    if (key == 'newest' || key == 'most') {
+        if (key == 'newest') {
+            data.sort((a, b) => new Date(b.ditemukan) - new Date(a.ditemukan))
+        } else {
+            data.sort((a, b) => (b.instrumen).length - (a.instrumen).length)
+        }
+    } else {
+        if (key == 'oldest') {
+            data.sort((a, b) => new Date(a.ditemukan) - new Date(b.ditemukan))
+        } else {
+            data.sort((a, b) => (a.instrumen).length - (b.instrumen).length)
+        }
+    }
+    return data
+}
+
+// Filter data berdasarkan key
+const filterItem = (data, key) => {
+    let result = [...data]
+    if (key != 'category-filter-all') {
+        if (key == 'category-filter-hageng') {
+            result = data.filter(element => element.jenis == 'hageng')
+        } else {
+            result = data.filter(element => element.jenis == 'pakurmatan')
+        }
+    }
+    return result
+}
+
+// Menambahkan Ke dalam Box Gallery
+const appendToContainer = data => {
+    document.querySelector(".gallery-content").innerHTML = ""
+    let heightIndex = 0
+    for (let i = 0; i < data.length; i++) {
+
+        let height = heightCard[heightIndex]
+        document.querySelector(".gallery-content").insertAdjacentHTML("beforeend", buildCardGallery(data[i].url, data[i].nama + " " + data[i].instrumen.length, data[i].jenis, data[i].ditemukan, height))
+
+        if (heightIndex == 2) {
+            heightIndex = 0
+            heightCard.reverse()
+        } else {
+            ++heightIndex
+        }
+
+    }
 
 
 
-// })
 
-fetch('./_data/gamelan.json').then(response => {
-    return response.json()
-}).then(data => {
-
-    data.forEach((element, index) => {
-        document.querySelector(".gallery-content").insertAdjacentHTML("beforeend", buildCardGallery(element.url, element.nama + index, element.jenis, element.ditemukan))
-    })
     FlexMasonry.init('.gallery-content', {
         responsive: true,
         breakpointCols: {
@@ -135,6 +211,48 @@ fetch('./_data/gamelan.json').then(response => {
         }
     })
     hoverCardInitialize()
-}).then(
-    console.log("ini jalan")
-)
+}
+
+// Funtion Apply Filter & Sort
+const applyFilter = () => {
+    let result = filterItem(globalDataGallery, filterMenu[0])
+    result = sortItem(result, filterMenu[1])
+    changingDataGallery = result
+    return result
+}
+
+// Event Listener untuk Apply button
+document.querySelector('.apply-button').addEventListener("click", function() {
+
+    let result = applyFilter()
+    console.log(result)
+    let paginationNumber = document.querySelector(".pagination-number")
+    paginationNumber.parentNode.replaceChild(initPageNumber(result.length), paginationNumber)
+    appendToContainer(paginationTest(1, result))
+})
+
+// })
+
+// Fetch Data dari JSON
+fetch('./_data/gamelan.json').then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data.length)
+    document.querySelector(".info-gallery").insertAdjacentElement("beforeend", initPageNumber(data.length))
+    let result = sortItem(data, "newest")
+    globalDataGallery = [...result]
+    result = paginationTest(1, result)
+    appendToContainer(result)
+    hoverCardInitialize()
+})
+
+// Memotong Data Per -- dari --
+const paginationTest = (index, data) => {
+    let total = data.length
+    let perPages = 10
+    let minPerPages = index * perPages - perPages
+    let maxPerPages = index * perPages > total ? total : index * perPages
+    document.querySelector(".pagination-info").innerText = `${minPerPages + 1} - ${maxPerPages} of ${total} Gamelan`
+    console.log(globalDataGallery.length)
+    return data.slice(minPerPages, maxPerPages)
+}
